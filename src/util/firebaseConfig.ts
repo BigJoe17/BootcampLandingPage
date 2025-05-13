417e33282e516f1873bf062f648ba95e2f3327c4
Bigjoe17 <forwardeverconsults23@gmail.com>
2025-05-13 12:35:26 +0530

    updated

--- src/util/firebaseConfig.ts
+++ src/util/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCI75ZEzk30UPu9RsVwaeuQX0_mjXCOTxk",
-  authDomain: "webdevbootcamp-f5306.firebaseapp.com",
-  projectId: "webdevbootcamp-f5306",
-  storageBucket: "webdevbootcamp-f5306.firebasestorage.app",
-  messagingSenderId: "53212666276",
-  appId: "1:53212666276:web:e6c4edbb841a0bdb8c4931"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 const app = initializeApp(firebaseConfig);
